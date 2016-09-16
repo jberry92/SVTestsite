f9f132caf4233c4a061cfc7a8372abf55a1d06e1
Joshua Berry <joshua.berry92@gmail.com>
2016-09-16 12:32:26 +0100

    Added materalize navbar, set up basic homescreen

---
+++ js/app.js
@@ -0,0 +1,30 @@
+$(function(){
+
+
+  $.get('https://api.themoviedb.org/3/tv/60573?api_key=4fbf35c945e96e2f343d00d0ddcea967' , function(data){
+    console.log(data);
+
+
+  })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
