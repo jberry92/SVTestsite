392b29f3ee5236b2a1b3c1014fa78f6f198d3ad8
Joshua Berry <joshua.berry92@gmail.com>
2016-09-16 14:12:21 +0100

    Added basic API functionality

--- js/app.js
+++ js/app.js
@@ -1,30 +1,38 @@
 $(function(){
 
+  function initialize(){
+    $seasonName = $('#title');
+    $airDate = $('#airDate');
+    $seasonOverview = $('#overview');
+    $seasonImages = $('#poster');
+  }
 
-  $.get('https://api.themoviedb.org/3/tv/60573?api_key=4fbf35c945e96e2f343d00d0ddcea967' , function(data){
-    console.log(data);
-
-
-  })
-
-
-
-
-
-
-
-
-
-
+  function addEventListeners(){
+    $('#seasonOne').on('click', function(){
+      return getSeasonData(1);
+    })
+    $('#seasonTwo').on('click', function(){
+      return getSeasonData(2);
+    })
 
 
+  }
+
+  function getSeasonData(season){
+    $.get('https://api.themoviedb.org/3/tv/60573/season/'+season+'?api_key=4fbf35c945e96e2f343d00d0ddcea967' , function(data){
+      console.log(data);
+      $seasonName.html(data.name);
+      $airDate.html(data.air_date);
+      $seasonOverview.html(data.overview);
+      $seasonImages.html("<img id='theImg' src= https://image.tmdb.org/t/p/w500/" + data.poster_path +" '/>");
+      return data;
+    })
+  }
 
 
 
 
 
-
-
-
-
+  initialize();
+  addEventListeners();
 })
