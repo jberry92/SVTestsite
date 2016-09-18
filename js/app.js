a5cef754669852b534a5af5423dcf87380d76639
Joshua Berry <joshua.berry92@gmail.com>
2016-09-18 12:03:26 +0100

    Added core functionality

--- js/app.js
+++ js/app.js
@@ -5,34 +5,63 @@ $(function(){
     $airDate = $('#airDate');
     $seasonOverview = $('#overview');
     $seasonImages = $('#poster');
+
   }
 
   function addEventListeners(){
     $('#seasonOne').on('click', function(){
-      return getSeasonData(1);
+      getSeasonData(1);
     })
     $('#seasonTwo').on('click', function(){
-      return getSeasonData(2);
+      getSeasonData(2);
     })
-
-
+    $('#seasonOneEpisodes').on('click', function(){
+      getSeasonData(1);
+    })
+    $('#seasonTwoEpisodes').on('click', function(){
+      getSeasonData(2);
+    })
   }
 
   function getSeasonData(season){
-    $.get('https://api.themoviedb.org/3/tv/60573/season/'+season+'?api_key=4fbf35c945e96e2f343d00d0ddcea967' , function(data){
+    var html;
+    var dataCall = $.get('https://api.themoviedb.org/3/tv/60573/season/'+season+'?api_key=4fbf35c945e96e2f343d00d0ddcea967' ,
+    function(data){
       console.log(data);
       $seasonName.html(data.name);
       $airDate.html(data.air_date);
       $seasonOverview.html(data.overview);
       $seasonImages.html("<img id='theImg' src= https://image.tmdb.org/t/p/w500/" + data.poster_path +" '/>");
-      return data;
+      episodes = data.episodes;
+      episodes.forEach(function(episode){
+        console.log(episode.name + " " + episode.overview);
+        html+= '<tr><td>' + episode.name + '</td><td>' + episode.overview + '</td><td>' + episode.air_date +'</td></tr>'
+      })
+      $('#episodeData').html(html);
     })
+
   }
 
+  function getSimilar(){
+      var html;
+      var dataCall = $.get('https://api.themoviedb.org/3/tv/60573/similar?api_key=4fbf35c945e96e2f343d00d0ddcea967',
+      function(data){
+        console.log(data);
+        similar = data.results;
+        similar.forEach(function(recommendation){
+          html+= '<tr><td>' + recommendation.name + '</td><td>' + recommendation.overview + '</td><td>' + recommendation.first_air_date +'</td></tr>'
+          console.log(recommendation.name +`\n`+recommendation.overview+'\n'+recommendation.first_air_date);
+        })
+        $('#similarProg').html(html);
+      });
+
+
+  }
 
 
 
 
   initialize();
   addEventListeners();
+  getSimilar();
 })
