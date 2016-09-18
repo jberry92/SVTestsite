90120c4720e3a3f97cca3d24b3bcbb59f53651fb
Joshua Berry <joshua.berry92@gmail.com>
2016-09-18 12:25:09 +0100

    Added more functionality, hiding sections, seperated out functions.

--- js/app.js
+++ js/app.js
@@ -1,67 +1,105 @@
-$(function(){
+$(function() {
 
-  function initialize(){
-    $seasonName = $('#title');
-    $airDate = $('#airDate');
-    $seasonOverview = $('#overview');
-    $seasonImages = $('#poster');
+    function initialize() {
+        $seasonName = $('#title');
+        $airDate = $('#airDate');
+        $seasonOverview = $('#overview');
+        $seasonImages = $('#poster');
+        $('#episodes').hide();
+        $('#seasons').hide();
+        $('#similar').hide();
 
-  }
+    }
 
-  function addEventListeners(){
-    $('#seasonOne').on('click', function(){
-      getSeasonData(1);
-    })
-    $('#seasonTwo').on('click', function(){
-      getSeasonData(2);
-    })
-    $('#seasonOneEpisodes').on('click', function(){
-      getSeasonData(1);
-    })
-    $('#seasonTwoEpisodes').on('click', function(){
-      getSeasonData(2);
-    })
-  }
-
-  function getSeasonData(season){
-    var html;
-    var dataCall = $.get('https://api.themoviedb.org/3/tv/60573/season/'+season+'?api_key=4fbf35c945e96e2f343d00d0ddcea967' ,
-    function(data){
-      console.log(data);
-      $seasonName.html(data.name);
-      $airDate.html(data.air_date);
-      $seasonOverview.html(data.overview);
-      $seasonImages.html("<img id='theImg' src= https://image.tmdb.org/t/p/w500/" + data.poster_path +" '/>");
-      episodes = data.episodes;
-      episodes.forEach(function(episode){
-        console.log(episode.name + " " + episode.overview);
-        html+= '<tr><td>' + episode.name + '</td><td>' + episode.overview + '</td><td>' + episode.air_date +'</td></tr>'
-      })
-      $('#episodeData').html(html);
-    })
-
-  }
-
-  function getSimilar(){
-      var html;
-      var dataCall = $.get('https://api.themoviedb.org/3/tv/60573/similar?api_key=4fbf35c945e96e2f343d00d0ddcea967',
-      function(data){
-        console.log(data);
-        similar = data.results;
-        similar.forEach(function(recommendation){
-          html+= '<tr><td>' + recommendation.name + '</td><td>' + recommendation.overview + '</td><td>' + recommendation.first_air_date +'</td></tr>'
-          console.log(recommendation.name +`\n`+recommendation.overview+'\n'+recommendation.first_air_date);
+    function addEventListeners() {
+        $('#seasonOne').on('click', function() {
+            getSeasonData(1);
         })
-        $('#similarProg').html(html);
-      });
+        $('#seasonTwo').on('click', function() {
+            getSeasonData(2);
+        })
+        $('#seasonOneEpisodes').on('click', function() {
+            getEpisodeData(1);
+        })
+        $('#seasonTwoEpisodes').on('click', function() {
+            getEpisodeData(2);
+        })
+        $('#season').on('click', function() {
+            $('#seasons').show();
+            $('#splashPage').hide();
+            $('#episodes').hide();
+            $('#similar').hide();
+        })
+        $('#episode').on('click', function() {
+            $('#episodes').show();
+            $('#splashPage').hide();
+            $('#seasons').hide();
+            $('#similar').hide();
+        })
+        $('#recommendations').on('click', function() {
+            getSimilar();
+            $('#similar').show();
+            $('#splashPage').hide();
+            $('#seasons').hide();
+            $('#episodes').hide();
+        })
+        $('#navBarLogo').on('click', function() {
+            $('#splashPage').show();
+            $('#episodes').hide();
+            $('#seasons').hide();
+            $('#similar').hide();
+        })
 
 
-  }
+    }
+
+    function getSeasonData(season) {
+        var dataCall = $.get('https://api.themoviedb.org/3/tv/60573/season/' + season + '?api_key=4fbf35c945e96e2f343d00d0ddcea967',
+            function(data) {
+                console.log(data);
+                $seasonName.html(data.name);
+                $airDate.html(data.air_date);
+                $seasonOverview.html(data.overview);
+                $seasonImages.html("<img id='theImg' src= https://image.tmdb.org/t/p/w500/" + data.poster_path + " '/>");
+            })
+
+    }
+
+    function getEpisodeData(season){
+      console.log('in the function')
+      var html;
+      var dataCall = $.get('https://api.themoviedb.org/3/tv/60573/season/' + season + '?api_key=4fbf35c945e96e2f343d00d0ddcea967',
+          function(data) {
+              episodes = data.episodes;
+              episodes.forEach(function(episode) {
+                  // console.log(episode.name + " " + episode.overview);
+                  html += '<tr><td>' + episode.name + '</td><td>' + episode.overview + '</td><td>' + episode.air_date + '</td></tr>'
+              })
+              $('#episodeData').html(html);
+          })
+
+
+    }
+
+    function getSimilar() {
+        var html;
+        var dataCall = $.get('https://api.themoviedb.org/3/tv/60573/similar?api_key=4fbf35c945e96e2f343d00d0ddcea967',
+            function(data) {
+                console.log(data);
+                similar = data.results;
+                similar.forEach(function(recommendation) {
+                    html += '<tr><td>' + recommendation.name + '</td><td>' + recommendation.overview + '</td><td>' + recommendation.first_air_date + '</td></tr>'
+                        // console.log(recommendation.name +`\n`+recommendation.overview+'\n'+recommendation.first_air_date);
+                })
+                $('#similarProg').html(html);
+            });
+
+
+    }
 
 
 
 
-  initialize();
-  addEventListeners();
-  getSimilar();
+    initialize();
+    addEventListeners();
 })
